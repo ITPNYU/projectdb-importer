aa1de1ac91342b664d5e3b4b68779029068d6809
Karl Ward <karl@ghostghost.net>
2014-05-08 09:55:49 -0400

    changed query --KW

--- template/projectdb_page.php
+++ template/projectdb_page.php
@@ -7,9 +7,17 @@ if (get_option('projectdb_api_url') && get_option('projectdb_api_key')) {
   if (preg_match('/\/$/', $projectdb_path) != 1) {
     $projectdb_path .= '/';
   }
-  $projectdb_path .= 'venue' . '/' . get_option('projectdb_venue')
-    . '/' . 'projects'
-    . '?' . '&'
+#  $projectdb_path .= 'venue' . '/' . get_option('projectdb_venue')
+#    . '/' . 'projects'
+#    . '?'
+#    . 'results_per_page=300' . '&'
+#    . 'key=' . get_option('projectdb_api_key');
+
+  $filters = array('filters' => array( 'name' => 'venue', 'op' => 'in', 'value' => 100 ) );
+
+  $projectdb_path .= 'project'
+    . '?'
+    . 'q=' . urlencode(json_encode($filters)) . '&'
     . 'results_per_page=300' . '&'
     . 'key=' . get_option('projectdb_api_key');
 
