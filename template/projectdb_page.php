e74580e03454bf0c17417ec3d354d2e05b5684a0
Karl Ward <karl@ghostghost.net>
2014-05-16 17:12:15 -0400

    debugging --KW

--- template/projectdb_page.php
+++ template/projectdb_page.php
@@ -13,30 +13,37 @@ if (get_option('projectdb_api_url') && get_option('projectdb_api_key')) {
     foreach ($all_posts as $p) {
       array_push($all_posts_id, get_post_meta($p->ID, 'project_id', TRUE));
     }
+    $all_projects_id = array();
+    foreach ($projectdb['objects'] as $p) {
+      array_push($all_projects_id, $p['project_id']);
+    }
     echo "all_posts_id: ";
     var_dump($all_posts_id);
     echo "<ul>\n";
     foreach ($projectdb['objects'] as $p) {
-      if (!in_array($p['project_id'], $all_posts_id)) {
-        $attach = get_posts(array('post_type' => 'attachment', 'post_parent' => $p->ID));
-        foreach ($attach as $a) {
-          $ret = wp_delete_attachment($a->ID, TRUE);
+      if (!in_array($p['project_id'], $all_projects_id)) {
+        $posts = get_posts(array('numberposts' => 1, 'meta_key' => 'project_id', 'meta_value' => $p['project_id']));
+        if (count($posts) > 0) {
+          $attach = get_posts(array('post_type' => 'attachment', 'post_parent' => $posts[0]->ID));
+          foreach ($attach as $a) {
+            $ret = wp_delete_attachment($a->ID, TRUE);
+            if ($ret) {
+              echo 'attachment deleted: ';
+            }
+            else {
+              echo 'attachment deletion failure: ';
+            }
+            echo $posts[0]->ID . ' for project ' . $p['project_id'] . "\n";
+          }
+          $ret = wp_delete_post($posts[0]->ID, TRUE);
           if ($ret) {
-            echo 'attachment deleted: ';
+            echo 'post deleted: ';
           }
           else {
-            echo 'attachment deletion failure: ';
+            echo 'post deletion failure: ';
           }
-          echo $p->ID . ' for project ' . $p['project_id'] . "\n";
+          echo $posts[0]->ID . ' for project ' . $p['project_id'] . "<br />\n";
         }
-        $ret = wp_delete_post($p->ID, TRUE);
-        if ($ret) {
-          echo 'post deleted: ';
-        }
-        else {
-          echo 'post deletion: ';
-        }
-        echo $p->ID . ' for project ' . $p['project_id'] . "<br />\n";
       }
       else {
         $post_id = projectdb_post($p);
