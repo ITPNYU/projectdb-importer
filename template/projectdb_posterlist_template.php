6458df5de34be9305eb75fb51da794ea464ebea1
Karl Ward <karl@ghostghost.net>
2014-05-16 13:11:38 -0400

    missing added --KW

--- template/projectdb_posterlist_template.php
+++ template/projectdb_posterlist_template.php
@@ -15,25 +15,36 @@ $posts = get_posts(array('numberposts' => -1));
 <div id="content">
   <table class="table table-striped">
     <tr>
-      <th>project title</th>
-      <th style='text-align:right'>links to:</th>
-      <th>poster</th>
-      <th>pdf of poster</th>
-      <th>webpage</th>
-      <th>edit project</th>
-      <th>missing</th>
+      <th>Project title</th>
+      <th>Poster</th>
+      <th>PDF of Poster</th>
+      <th>Project Post</th>
+      <th>Edit Project</th>
+      <th>Missing</th>
     </tr>
 <?php
 foreach ($posts as $p) {
   $posterlink = get_site_url() . '/posterprint?post=' . $p->ID;
   $project_id = get_post_meta($p->ID, 'project_id', TRUE);
+  $fields = array('student', 'instructor', 'class', 'url', 'elevator_pitch', 'description');
+  $missing = array();
+  foreach ($fields as $f) {
+    $v = get_post_meta($p->ID, $f, TRUE);
+    if (!isset($v)) {
+      array_push($missing, $f);
+    }
+  }
+  // check for empty URL
+  // check for image attachment
+
 ?>
     <tr>
-      <td colspan="2"><?php echo $p->post_title; ?></td>
+      <td><?php echo $p->post_title; ?></td>
       <td><a href="<?php echo $posterlink; ?>">poster</a></td>
       <td><a href="/makepdf/?url=<?php echo $posterlink; ?>">PDF</a></td>
       <td><a href="<?php echo get_permalink($p); ?>">project post</a></td>
       <td><a href="https://itp.nyu.edu/projects/projectinfo.php?project_id=<?php echo project_id; ?>">Edit Project</a></td>
+      <td></td>
     </tr>
 <?php
 }
