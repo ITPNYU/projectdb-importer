a1447d43d9aecb554e98e03c9fa71727a34aba9b
Karl Ward <karl@ghostghost.net>
2014-05-16 13:03:20 -0400

    added posterlist fields --KW

--- template/projectdb_posterlist_template.php
+++ template/projectdb_posterlist_template.php
@@ -9,7 +9,7 @@ $posts = get_posts(array('numberposts' => -1));
 <h1>links to poster and webpage for each project</h1>
 <?php echo count($posts); ?> projects found
 <div id="content">
-  <table>
+  <table border="1">
     <tr>
       <th>project title</th>
       <th style='text-align:right'>links to:</th>
@@ -22,13 +22,14 @@ $posts = get_posts(array('numberposts' => -1));
 <?php
 foreach ($posts as $p) {
   $posterlink = get_site_url() . '/posterprint?post=' . $p->ID;
+  $project_id = get_post_meta($p->ID, 'project_id', TRUE);
 ?>
     <tr>
       <td colspan="2"><?php echo $p->post_title; ?></td>
       <td><a href="<?php echo $posterlink; ?>">poster</a></td>
       <td><a href="/makepdf/?url=<?php echo $posterlink; ?>">PDF</a></td>
       <td><a href="<?php echo get_permalink($p); ?>">project post</a></td>
-      <td><a href="https://itp.nyu.edu/projects/projectinfo.php?project_id=<?php echo get_post_meta($p->ID, 'project_id', TRUE); ?>">Edit Project</a></td>
+      <td><a href="https://itp.nyu.edu/projects/projectinfo.php?project_id=<?php echo project_id; ?>">Edit Project</a></td>
     </tr>
 <?php
 }
